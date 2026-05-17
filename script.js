5de0a5200b9fb72755889c946f2e6c27887f087a
Nathan <badumisanathan807@gmail.com>
2026-05-17 22:42:01 +0200

    Ajout de Firebase fonctionnel

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC6JiynxWiPQVjqZ-UMGpSyI9f_aDqxEGc",
+    authDomain: "nathan-trading.firebaseapp.com",
+    projectId: "nathan-trading",
+    storageBucket: "nathan-trading.firebasestorage.app",
+    messagingSenderId: "908084098772",
+    appId: "1:908084098772:web:c99392d2e52a10f1e7ed41"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
+
 console.log("Nathan Trading Premium Website Loaded");
